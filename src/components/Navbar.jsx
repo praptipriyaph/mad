6fbaab01454acc07e22f7ab63de7832f0387bb7d
Praptipriya Phukon <phukonpraptipriya@gmail.com>
2024-11-20 01:43:16 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import '..styles//Navbar.css';
+import '../styles/Navbar.css';
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <img src="logo.png" alt="Pharmacy Logo" className="logo" />
-        <span>Pharmacy</span>
+        <img src="../assets/logo.png" alt="Pharmacy Logo" className="logo" />
+        <span>PharmaFind</span>
       </div>
       <ul className="navbar-links">
         <li>Home</li>
         <li>About Us</li>
-        <li>Products</li>
-        <li>Services</li>
-        <li>Pricing</li>
-        <li>Contact Us</li>
+        <li>Cart</li>
+        <li>Log in</li>
       </ul>
-      <button className="contact-btn">+62 864 6444 2222</button>
     </nav>
   );
 };
